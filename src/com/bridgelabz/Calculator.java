9503d354fb2f80f3721de90b18ac5f3cc0ebc717
pa1zore <pavanzore345@gmail.com>
2023-12-05 11:41:15 +0530

    Added: Addition

--- src/com/bridgelabz/Calculator.java
+++ src/com/bridgelabz/Calculator.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("welcome to calculator");
+        addition();
+    }
+    public static void addition()
+    {
+        int a=10,b=20;
+        System.out.println(a+b);
     }
 }
