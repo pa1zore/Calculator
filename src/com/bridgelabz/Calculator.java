8167b80f28cc284570246db69dfe6666d6963fc4
pa1zore <pavanzore345@gmail.com>
2023-12-05 11:49:36 +0530

    Added: substraction

--- src/com/bridgelabz/Calculator.java
+++ src/com/bridgelabz/Calculator.java
@@ -4,10 +4,17 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("welcome to calculator");
         addition();
+        substraction();
+
     }
     public static void addition()
     {
         int a=10,b=20;
         System.out.println(a+b);
+    }
+    public static void substraction()
+    {
+        int a=10,b=20;
+        System.out.println(b-a);
     }
 }
