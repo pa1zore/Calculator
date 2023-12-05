6d5dad6b6d472ace3afed9cc9a7a1482672bb6f5
pa1zore <pavanzore345@gmail.com>
2023-12-05 12:06:02 +0530

    Added: division

--- src/com/bridgelabz/Calculator.java
+++ src/com/bridgelabz/Calculator.java
@@ -6,21 +6,27 @@ public class Calculator {
         addition();
         substraction();
         multiplication();
+        division();
 
     }
     public static void addition()
     {
         int a=10,b=20;
-        System.out.println(a+b);
+        System.out.println("Addition is "+(a+b));
     }
     public static void substraction()
     {
         int a=10,b=20;
-        System.out.println(b-a);
+        System.out.println("Substraction is "+(b-a));
     }
     public static void multiplication()
     {
         int a=10,b=20;
-        System.out.println(b*a);
+        System.out.println("Multiplication is "+(b*a));
+    }
+    public static void division()
+    {
+        int a=10,b=20;
+        System.out.println("Division is "+(b/a));
     }
 }
